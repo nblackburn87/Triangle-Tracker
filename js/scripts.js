ed1ee837016c439eb0d14677a74b845740976163
Natalie Blackburn & Jason Mannon <student@epicodus.com>
2014-02-24 15:21:15 -0800

    add tests for other triangle types

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,45 @@
 var Triangle = {
+
+  invalidTriangle: function() {
+    if ((this.firstSide > (this.secondSide + this.thirdSide)) || (this.secondSide > (this.firstSide + this.thirdSide)) || (this.thirdSide > (this.secondSide + this.firstSide))) {
+      return "Not a Triangle";
+    }    
+  },
+
   triangleTester: function() {
     var sideTotal = (((this.firstSide + this.secondSide) / this.thirdSide));
     
     if (sideTotal === 2) {
-      return ("Equilateral");
-    } else if ((this.firstSide > (this.secondSide + this.thirdSide)) || (this.secondSide > (this.firstSide + this.thirdSide)) || (this.thirdSide > (this.secondSide + this.firstSide))); {
-      alert("This Ran!");
-      return "Not a Triangle";
+      return "Equilateral";
+    } else if ((this.firstSide === this.secondSide) || (this.secondSide === this.thirdSide) || (this.thirdSide === this.firstSide)) {
+      return "Isosceles";
+    } else {
+      return "Scalene";
     }
   }
 };
 
+$(document).ready(function() {
+  $("form#triangle-sides").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSideOne = parseInt($("input#new-side-one").val());
+    var inputtedSideTwo = parseInt($("input#new-side-two").val());
+    var inputtedSideThree = parseInt($("input#new-side-three").val());
+    var newTriangle = Object.create(Triangle);
+    newTriangle.firstSide = inputtedSideOne;
+    newTriangle.secondSide = inputtedSideTwo;
+    newTriangle.thirdSide = inputtedSideThree;
+
+
+    if (newTriangle.invalidTriangle() === "Not a Triangle") {
+      alert("That is not a triangle");
+    } else if (newTriangle.triangleTester() === "Equilateral") {
+      $("#equilateral").prepend("<li>" + inputtedSideOne + ", " + inputtedSideTwo + ", " + inputtedSideThree + "</li>")
+    } else if (newTriangle.triangleTester() === "Isosceles") {
+      $("#isosceles").prepend("<li>" + inputtedSideOne + ", " + inputtedSideTwo + ", " + inputtedSideThree + "</li>")
+    } else if (newTriangle.triangleTester() === "Scalene") {
+      $("#scalene").prepend("<li>" + inputtedSideOne + ", " + inputtedSideTwo + ", " + inputtedSideThree + "</li>")
+    }
+  });
+});
--- specs/specs.js
+++ specs/specs.js
@@ -2,17 +2,31 @@ describe("Triangle Tracker", function() {
   describe("triangleTester", function() {
     it("Takes 3 sides of a triangle and returns Equilateral", function() {
       var testTriangle = Object.create(Triangle);
-      testTriangle.firstSide = 4;
-      testTriangle.secondSide = 4;
-      testTriangle.thirdSide = 4;
+      testTriangle.firstSide = 3;
+      testTriangle.secondSide = 3;
+      testTriangle.thirdSide = 3;
       testTriangle.triangleTester().should.equal("Equilateral");
     });
-    it("Takes 3 inputs that do make a triangle and returns 'Not a Triangle'", function() {
+    it("Takes 3 inputs that do not make a triangle and returns 'Not a Triangle'", function() {
       var testTriangle = Object.create(Triangle);
       testTriangle.firstSide = 4;
       testTriangle.secondSide = 6;
       testTriangle.thirdSide = 12;
-      testTriangle.triangleTester().should.equal("Not a Triangle");
+      testTriangle.invalidTriangle().should.equal("Not a Triangle");
+    });
+    it("If two inputs are the same and make a triangle, return Isosceles", function() {
+      var testTriangle = Object.create(Triangle);
+      testTriangle.firstSide = 4;
+      testTriangle.secondSide = 4;
+      testTriangle.thirdSide = 6;
+      testTriangle.triangleTester().should.equal("Isosceles");
+    });
+    it("If the inputs are all different lengths and make a triangle, return Scalene", function() {
+      var testTriangle = Object.create(Triangle);
+      testTriangle.firstSide = 10;
+      testTriangle.secondSide = 12;
+      testTriangle.thirdSide = 14;
+      testTriangle.triangleTester().should.equal("Scalene");
     });
   });
 });

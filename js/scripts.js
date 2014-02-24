9d012ba48c0b7ce4b3e1caf6cddd664f33806814
Natalie Blackburn & Jason Mannon <student@epicodus.com>
2014-02-24 11:28:51 -0800

    create test for Non-Triangle Inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 var Triangle = {
   triangleTester: function() {
     var sideTotal = (((this.firstSide + this.secondSide) / this.thirdSide));
-   
+    
     if (sideTotal === 2) {
       return ("Equilateral");
-    } else {
-      return false;
+    } else if ((this.firstSide > (this.secondSide + this.thirdSide)) || (this.secondSide > (this.firstSide + this.thirdSide)) || (this.thirdSide > (this.secondSide + this.firstSide))); {
+      alert("This Ran!");
+      return "Not a Triangle";
     }
-   }
+  }
 };
+
--- specs/specs.js
+++ specs/specs.js
@@ -7,5 +7,12 @@ describe("Triangle Tracker", function() {
       testTriangle.thirdSide = 4;
       testTriangle.triangleTester().should.equal("Equilateral");
     });
+    it("Takes 3 inputs that do make a triangle and returns 'Not a Triangle'", function() {
+      var testTriangle = Object.create(Triangle);
+      testTriangle.firstSide = 4;
+      testTriangle.secondSide = 6;
+      testTriangle.thirdSide = 12;
+      testTriangle.triangleTester().should.equal("Not a Triangle");
+    });
   });
 });
